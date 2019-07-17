4f7aaed4aa3bfb45e86af2416632bd87d0588968
Devin Walter <jwaltede@jci.com>
2019-07-17 11:02:40 -0400

    started adding dashboard, moving declarations out

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero'; 
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroSevice: fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
